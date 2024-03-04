c79bc8f5e7f52411c9cd8bc56afa52b796803aae
hbaltz <hugobaltz@gmail.com>
2024-03-04 19:14:12 +0100

    Create a filter with ListCrudRepository

--- src/main/java/dev/hbaltz/contentcalendar/content/controller/ContentJdbcDataController.java
+++ src/main/java/dev/hbaltz/contentcalendar/content/controller/ContentJdbcDataController.java
@@ -64,6 +64,9 @@ public class ContentJdbcDataController {
         if (!contentRepository.existsById(id)) {
             throw new ContentNotFoundresponseStatusException(id);
         }
+        // I do not like of this way of working. For me, we should have a method to save the content and one to update it
+        // Having a method that create a new content in the database if the id is not present in the database and that also
+        // Update a content if existing in the database is not that predictable when reading the code
         contentRepository.save(content);
     }
 
@@ -77,5 +80,18 @@ public class ContentJdbcDataController {
             throw new ContentNotFoundresponseStatusException(id);
         }
         contentRepository.deleteById(id);
+    }
+
+    /**
+     * @param keyword the keyword to filter the title (Warning it is case sensitive)
+     * @return all contents with the title containing the keyword
+     *
+     * I wouldn't do that like that to filter my content,
+     * I would prefer to pass get parameters to have different filters applied at the same time
+     * At moment I would continue to follow the tutorial
+     */
+    @GetMapping("/filter/{keyword}")
+    public List<Content> findContentListByTitle(@PathVariable String keyword) {
+        return contentRepository.findAllByTitleContains(keyword);
     }
 }
--- src/main/java/dev/hbaltz/contentcalendar/content/repository/ContentRepository.java
+++ src/main/java/dev/hbaltz/contentcalendar/content/repository/ContentRepository.java
@@ -3,5 +3,9 @@ package dev.hbaltz.contentcalendar.content.repository;
 import dev.hbaltz.contentcalendar.content.model.Content;
 import org.springframework.data.repository.ListCrudRepository;
 
+import java.util.List;
+
 public interface ContentRepository extends ListCrudRepository<Content,Integer> {
+
+    List<Content> findAllByTitleContains(String keyword);
 }
