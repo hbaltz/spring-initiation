64dfa788acf8c0ff04c34dcc7b3813c081d5b6f2
hbaltz <hugobaltz@gmail.com>
2024-03-06 17:14:33 +0100

    Create a home controller and an api to display custom properties defined in the project

---
+++ src/main/java/dev/hbaltz/contentcalendar/home/controller/HomeController.java
@@ -0,0 +1,31 @@
+package dev.hbaltz.contentcalendar.home.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+/**
+ * Used to controls api for home page
+ */
+@RestController
+public class HomeController {
+
+    @Value("${cc.welcomeMessage: Default Welcome Message}")
+    private String welcomeMessage;
+
+    @Value("${cc.about: Default About}")
+    private String about;
+
+    /**
+     * @return the welcome message
+     */
+    @GetMapping("/")
+    public Map<String,String> displayWelcomeMessage() {
+        return Map.of(
+                "welcomeMessage", welcomeMessage,
+                "about", about
+        );
+    }
+}
